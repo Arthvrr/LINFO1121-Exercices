83139e3761fdf4d251cf738c521f94dfd2ed37e9
Arhtvrr <arthurlouette12@gmail.com>
2025-08-04 12:41:15 +0200

    Succeeded FList

---
+++ Partie 1 - ADT/FList/src/main/java/fundamentals/FList.java
@@ -0,0 +1,158 @@
+package fundamentals;
+
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * Author: Pierre Schaus
+ *
+ * Functional Programming is an increasingly important programming paradigm.
+ * In this programming paradigm, data structures are immutable.
+ * We are interested here in the implementation of an immutable list
+ * called FList allowing to be used in a functional framework.
+ * Look at the main method for a small example using this functional list
+ *
+ * Complete the implementation to pass the tests
+ *
+ * @param <A>
+ */
+public abstract class FList<A> implements Iterable<A> {
+
+    public static void main(String[] args) {
+        FList<Integer> list = FList.nil();
+
+        for (int i = 0; i < 10; i++) {
+            list = list.cons(i);
+        }
+
+        list = list.map(i -> i+1);
+        // will print 10,9,...,1
+        for (Integer i: list) {
+            System.out.println(i);
+        }
+
+        list = list.filter(i -> i%2 == 0);
+        // will print 10,...,6,4,2
+        for (Integer i: list) {
+            System.out.println(i);
+        }
+    }
+
+    // return true if the list is not empty, false otherwise
+    public final boolean isNotEmpty() {
+        return this instanceof Cons;
+    }
+
+    // return true if the list is empty (Nill), false otherwise
+    public final boolean isEmpty() {
+        return this instanceof Nil;
+    }
+
+    // return the length of the list
+    public final int length() {
+        // TODO
+        return 1 + tail().length(); //récursif, 1 + la taille de la queue
+    }
+
+    // return the head element of the list
+    public abstract A head();
+
+    // return the tail of the list
+    public abstract FList<A> tail();
+
+    // creates an empty list
+    public static <A> FList<A> nil() {
+        return (Nil<A>) Nil.INSTANCE;
+    }
+
+
+    public final FList<A> cons(final A a) {
+        return new Cons(a, this);
+    }
+
+    // return a list on which each element has been applied function f
+    public final <B> FList<B> map(Function<A,B> f) {
+        // TODO
+        if (isEmpty()) return nil(); //si vide on retourne nil
+        return tail().map(f).cons(f.apply(head()));
+    }
+
+    // return a list on which only the elements that satisfies predicate are kept
+    public final FList<A> filter(Predicate<A> f) {
+        // TODO
+        if (isEmpty()) return nil(); //si vide on retourne nil
+        if (f.test(head())) return tail().filter(f).cons(head()); //si respecte le filtre on print avec cons()
+        return tail().filter(f);
+    }
+
+
+    // return an iterator on the element of the list
+    public Iterator<A> iterator() {
+        return new Iterator<A>() {
+            // complete this class
+
+            FList<A> current = FList.this;
+
+            public boolean hasNext() {
+                // TODO
+                return current.isNotEmpty();
+            }
+
+            public A next() {
+                // TODO
+                A next = current.head();
+                current = current.tail(); //on passe à l'élément suivant
+                return next; //on retourne la head actuelle
+            }
+
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+
+    private static final class Nil<A> extends FList<A> {
+        public static final Nil<Object> INSTANCE = new Nil();
+
+        @Override
+        public A head() {
+            // TODO
+            throw new IllegalArgumentException(); //Nil, n'a pas de head ni tail
+        }
+
+        @Override
+        public FList<A> tail() {
+            // TODO
+            throw new IllegalArgumentException();//Nil, n'a pas de head ni tail
+        }
+    }
+
+    private static final class Cons<A> extends FList<A> {
+
+        // TODO add instance variables
+        private A a;
+        private FList<A> tail;
+
+
+        Cons(A a, FList<A> tail) {
+            this.a = a;
+            this.tail = tail;
+        }
+
+        @Override
+        public A head() {
+            // TODO
+            return a;
+        }
+
+        @Override
+        public FList<A> tail() {
+            // TODO
+            return tail;
+        }
+    }
+}
