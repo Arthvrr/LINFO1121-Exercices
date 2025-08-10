36461efc2ba05cc361b33463a5c5959cc4111f44
Arhtvrr <arthurlouette12@gmail.com>
2025-08-10 12:05:34 +0200

    Succeeded ArrayBST

--- Partie 3 - Search Trees/ArrayBST/src/main/java/searching/ArrayBST.java
+++ Partie 3 - Search Trees/ArrayBST/src/main/java/searching/ArrayBST.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
  *  The right node of node i, if any is located
  *       at index idxRightNode.get(i) otherwise idxRightNode.get(i) == NONE
  *
- *  The tree below is the result of putting (key,value) pairs (12,A),(15,B),(5,C),(8,d),(1,E) (in this order)
+ *  The tree below is the result of putting (key,value) pairs (12,A),(15,B),(5,C),(8,D),(1,E) (in this order)
  *
  *                12(A)
  *                / \
@@ -64,7 +64,51 @@ public class ArrayBST<Key extends Comparable<Key>, Value> {
      * @return true if the key was added, false if already present and the value has simply been erased
      */
     public boolean put(Key key, Value val) {
-         return false;
+        //TODO
+
+        if (keys.isEmpty()){ //si arbre vide au départ
+            keys.add(key); //insertion en fin de liste pour les 4 listes
+            values.add(val);
+            idxLeftNode.add(NONE);
+            idxRightNode.add(NONE);
+            return true; //clé insérée comme convenu
+        }
+        else { //arbre pas vide
+
+            if (keys.contains(key)){ //si la clé est déjà présente dans l'arbre, on doit juste remplacer par la nouvelle valeur
+                int idxKey = keys.indexOf(key); //on récupère l'index correct de la clé
+                values.set(idxKey,val); //on remplace l'ancienne valeur par la nouvelle (val)
+                return false; //clé non-insérée, false
+
+            } else { //la clé n'est pas présente dans l'arbre, on doit l'insérer
+                keys.add(key); //insertion en fin de liste pour les 4 listes
+                values.add(val);
+                idxLeftNode.add(NONE);
+                idxRightNode.add(NONE);
+
+                //on doit maintenant changer la valeur de l'index du fils gauche ou fils droit pour le noeud parent du nouveau noeud
+                int currentIndex = 0;
+                int idxToPut = values.size()-1; //valeur à changer dans idxLeftNode ou idxRightNode
+
+                while (true) {
+                    Key currentRoot = keys.get(currentIndex);
+
+                    if (key.compareTo(currentRoot) > 0) { //si la clé est plus grande que la racine, sous-arbre droit
+                        if (idxRightNode.get(currentIndex) == NONE){ //place libre à droite, on insère ici
+                            idxRightNode.set(currentIndex,idxToPut); //on met à jour l'index du fils gauche
+                            break;
+                        } else currentIndex = idxRightNode.get(currentIndex);
+
+                    } else { //si la clé est plus petite que la racine, sous-arbre gauche
+                        if (idxLeftNode.get(currentIndex) == NONE){ //place libre à gauche, on insère ici
+                            idxLeftNode.set(currentIndex,idxToPut); //on met à jour l'index du fils droit
+                            break;
+                        } else currentIndex = idxLeftNode.get(currentIndex);
+                    }
+                }
+            }
+            return true; //clé bien insérée comme convenu
+        }
     }
 
     /**
@@ -74,10 +118,10 @@ public class ArrayBST<Key extends Comparable<Key>, Value> {
      * @return the value attached to this key, null if the key is not present
      */
     public Value get(Key key) {
-         return null;
+        //TODO
+        if (keys.isEmpty()) return null; //si arbre vide retourner null
+        int indexKey = keys.indexOf(key); //on récupère l'index correct de la clé
+        if (indexKey == -1) return null; //si l'index retourné est -1, la clé n'est pas présente on retourne null
+        return values.get(indexKey);
     }
-
-
-
-
 }
